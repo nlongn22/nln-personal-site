58a8ea4213e6dfd5fc7b44fdc1c9516faec3bf48
Ngoc Long Nguyen <87426699+nlongn22@users.noreply.github.com>
2023-07-16 02:14:51 +0200

    Remove already included eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,6 @@ module.exports = {
         'space-before-function-paren': ['error', 'never'],
         'no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 1, 'maxBOF': 0 }],
         'vue/html-indent': ['error', 4],
-        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-        'vue/max-attributes-per-line': 1,
         'vue/multi-word-component-names': 0,
     },
 };
